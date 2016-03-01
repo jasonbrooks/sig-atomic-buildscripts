fea82057e69c0a805b6e1235b01cfac11c6a1371
Jason Brooks <jbrooks@redhat.com>
2016-03-01 12:59:39 -0800

    foo

--- build_ostree_components.sh
+++ build_ostree_components.sh
@@ -32,7 +32,7 @@ set -o pipefail
 
 ## update script from git, commented out for now
 cd ${BuildDir}
-git clone https://github.com/CentOS/sig-atomic-buildscripts && cd sig-atomic-buildscripts && git checkout devel
+git clone https://github.com/jasonbrooks/sig-atomic-buildscripts && cd sig-atomic-buildscripts && git checkout docker-test
 cd ${BuildDir}
 
 # Init, make sure we have the bits we need installed. 
