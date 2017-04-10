2e4ba07eff9c3ef24c311b0ea2d36274b0a0067f
Jason Brooks <jbrooks@redhat.com>
2017-04-10 15:00:13 -0700

    temp build from signed tree location

--- build_stage1.sh
+++ build_stage1.sh
@@ -32,7 +32,7 @@ set -o pipefail
 
 ## update script from git, commented out for now
 cd ${BuildDir}
-git clone https://github.com/CentOS/sig-atomic-buildscripts && cd sig-atomic-buildscripts && git checkout downstream
+git clone https://github.com/jasonbrooks/sig-atomic-buildscripts && cd sig-atomic-buildscripts && git checkout 7.20170410
 cd ${BuildDir}
 
 # Init, make sure we have the bits we need installed. 
@@ -60,23 +60,23 @@ fi
 
 # sync repo from ds location
 
-ostree remote add --repo=/srv/repo centos-atomic-host --set=gpg-verify=false http://mirror.centos.org/centos/7/atomic/x86_64/repo && ostree pull --depth=-1 --repo=/srv/repo --mirror centos-atomic-host centos-atomic-host/7/x86_64/standard
+ostree remote add --repo=/srv/repo centos-atomic-host --set=gpg-verify=true https://buildlogs.centos.org/centos/7/atomic/x86_64/repo/ && ostree pull --depth=-1 --repo=/srv/repo --mirror centos-atomic-host centos-atomic-host/7/x86_64/standard
 
 # temp fix for issue 259
 
-ostree --repo=/srv/repo static-delta generate --from d433342b09673c9c4d75ff6eef50a447e73a7541491e5197e1dde14147b164b8 --to 841fae51e5b68716a9996ddbdb4e543855bbfab9c6e4cb433267b24e41e8bbc1 && ostree --repo=/srv/repo summary -u
+#ostree --repo=/srv/repo static-delta generate --from d433342b09673c9c4d75ff6eef50a447e73a7541491e5197e1dde14147b164b8 --to 841fae51e5b68716a9996ddbdb4e543855bbfab9c6e4cb433267b24e41e8bbc1 && ostree --repo=/srv/repo summary -u
 
 ## compose a new tree, based on defs in centos-atomic-host.json
 
-rpm-ostree compose --repo=${OstreeRepoDir} tree ${GitDir}/centos-atomic-host.json |& tee ${BuildDir}/log.compose
-if ostree --repo=${OstreeRepoDir} rev-parse centos-atomic-host/7/x86_64/standard^ &>/dev/null; then
-    ostree --repo=${OstreeRepoDir} static-delta generate centos-atomic-host/7/x86_64/standard
-fi
-ostree --repo=${OstreeRepoDir} summary -u |& tee ${BuildDir}/log.compose
+#rpm-ostree compose --repo=${OstreeRepoDir} tree ${GitDir}/centos-atomic-host.json |& tee ${BuildDir}/log.compose
+#if ostree --repo=${OstreeRepoDir} rev-parse centos-atomic-host/7/x86_64/standard^ &>/dev/null; then
+#    ostree --repo=${OstreeRepoDir} static-delta generate centos-atomic-host/7/x86_64/standard
+#fi
+#ostree --repo=${OstreeRepoDir} summary -u |& tee ${BuildDir}/log.compose
 
 # deal with https://bugzilla.gnome.org/show_bug.cgi?id=748959
 
-chmod -R a+r /srv/repo/objects
+#chmod -R a+r /srv/repo/objects
 
 echo 'Stage-1 done, you can now sign the repo, or just run stage2 '
 
