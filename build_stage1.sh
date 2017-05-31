15be5c477eb303e68586a400868b4e9aa664b55e
Jason Brooks <jbrooks@redhat.com>
2017-05-30 18:18:40 -0700

    test

--- build_stage1.sh
+++ build_stage1.sh
@@ -39,7 +39,7 @@ cd ${BuildDir}
 cp -f ${GitDir}/rhel-atomic-rebuild.repo /etc/yum.repos.d/
 yum -y install ostree rpm-ostree glib2 docker libvirt epel-release libgsystem
 
-cp -f ${GitDir}/atomic7-testing.repo /etc/yum.repos.d/
+mv ${GitDir}/atomic7-testing.repo /etc/yum.repos.d/
 echo 'enabled=0' >> /etc/yum.repos.d/atomic7-testing.repo
 yum --enablerepo=atomic7-testing -y install rpm-ostree-toolbox
 
